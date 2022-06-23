5f686afa475da981ea90483db91d642854bd55c0
unknown <fernandaaflah@gmail.com>
2022-06-23 08:56:07 +0700

    clear comment list movie

--- src/screen/List/index.js
+++ src/screen/List/index.js
@@ -193,28 +193,6 @@ function ListScreen(props) {
   };
 
   console.log(refresh);
-  // const [movie, setMovie] = useState([]);
-  // const [pageInfo, setPageInfo] = useState([]);
-  // useEffect(() => {
-  //   handleGetMovie();
-  // }, []);
-  // const handleGetMovie = async () => {
-  //   try {
-  //     const result = await axios.get(
-  //       'movie?page=1&limit=10&sort=&searchRelease=&searchName=',
-  //     );
-  //     setMovie(result.data.data);
-  //     setPageInfo(result.data.pagination);
-  //   } catch (error) {
-  //     console.log(error.response.data);
-  //   }
-  // };
-  // const handleDetail = id => {
-  //   props.navigation.navigate('DetailScreen', {
-  //     screen: 'Detail',
-  //     params: {id: id},
-  //   });
-  // };
   return (
     <View style={styles.row}>
       <FlatList
@@ -252,73 +230,6 @@ function ListScreen(props) {
         }
       />
     </View>
-    // <ScrollView>
-    //   <View style={styles.container}>
-    //     <Text style={styles.nowShowing}>List Movie</Text>
-    //   </View>
-    //   <View style={styles.flex}>
-    //     <View style={styles.flex1}>
-    //       <Text style={styles.sort}>Sort</Text>
-    //     </View>
-    //     <View style={styles.flex2}>
-    //       <TextInput
-    //         placeholder="search movie name.."
-    //         style={styles.textInputBoxs}
-    //       />
-    //     </View>
-    //   </View>
-    //   <ScrollView style={styles.scroll} horizontal>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>January</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>February</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>March</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>May</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>June</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>July</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>August</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>September</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>October</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>November</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.button}>
-    //       <Text style={styles.buttonText}>December</Text>
-    //     </TouchableOpacity>
-    //   </ScrollView>
-    //   <View style={styles.row}>
-    //     {movie.map(item => (
-    //       <View style={styles.cardUpcoming} key={item.id}>
-    //         <Image
-    //           source={require('../../assets/spiderman.png')}
-    //           style={styles.imageCard}
-    //         />
-    //         <Text style={styles.movieText}>{item.name}</Text>
-    //         <Text style={styles.movieText2}>{item.category}</Text>
-    //         <TouchableOpacity style={styles.buttonUpcoming}>
-    //           <Text style={styles.buttonViewAllText}>Detail</Text>
-    //         </TouchableOpacity>
-    //       </View>
-    //     ))}
-    //   </View>
-    //   <Footer />
-    // </ScrollView>
   );
 }
 
